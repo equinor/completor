7ee691fa2087193c4cd9e468b610a0487cedd0ea
Atle Johnsen Gyllensten <60503777+atlejg@users.noreply.github.com>
2021-01-14 13:39:21 +0100

    rewritten-history-with-blame

--- tests/completor/test_drogon.py
+++ tests/completor/test_drogon.py
@@ -7,3 +7,18 @@ import pytest
 
 _TESTDIR_DROGON = Path(__file__).absolute().parent / "data" / "drogon"
 _TEST_FILE = "test.sch"
+
+
+with open(_TESTDIR_DROGON / "test_cases.files", encoding="utf-8") as file:
+    CASE_FILES = file.readlines()
+
+
+@pytest.mark.parametrize(
+    "drogon_case",
+    [pytest.param(line.strip("\n")) for line in CASE_FILES if not line.startswith("--")],
+)
+def test_drogons(drogon_case, tmpdir):
+    """Test Completor with Drogon cases."""
+
+    # Copy pvt file to tmpdir before creating schedule files
+    tmpdir.chdir()
