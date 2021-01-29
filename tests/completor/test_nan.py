6026faafa518e1da875f8059ca73ef8c929b1a98
Trygve Kløv <36536100+tklov11@users.noreply.github.com>
2021-01-29 15:46:15 +0100

    rewritten-history-with-blame

--- tests/completor/test_nan.py
+++ tests/completor/test_nan.py
@@ -2,3 +2,17 @@
 
 from pathlib import Path
 
+import common
+
+_TESTDIR = Path(__file__).absolute().parent / "data"
+_TEST_FILE = "ml_well.sch"
+
+
+def test_nan(tmpdir):
+    """
+    Test that the correct amount of output elements are produced with changing input.
+
+    Completor should produce 1* in rows of 13 elements in the COMPDAT output when the
+    input number of columns changes between 13 and 14.
+    """
+    tmpdir.chdir()
