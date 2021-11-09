5048643934c1556ee5b18c4678329461754778d9
Sindre Nistad <snis@equinor.com>
2021-11-09 12:00:16 +0100

    rewritten-history-with-blame

--- completor/create_output.py
+++ completor/create_output.py
@@ -10,3 +10,4 @@ import matplotlib  # type: ignore
 from completor import prepare_outputs as po
 from completor.completion import WellSchedule
 from completor.create_wells import CreateWells
+from completor.logger import logger
