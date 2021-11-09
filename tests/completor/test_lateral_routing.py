a68bcf0bdb44e4a782fc1362f511c7a948877ef9
Sindre Nistad <snis@equinor.com>
2021-11-09 15:45:47 +0100

    rewritten-history-with-blame

--- tests/completor/test_lateral_routing.py
+++ tests/completor/test_lateral_routing.py
@@ -31,3 +31,44 @@ WSEGAICD = """
 WSEGAICD
 --Number    Alpha       x   y   a   b   c   d   e   f   rhocal  viscal
 1           0.00021   0.0   1.0 1.1 1.2 0.9 1.3 1.4 2.1 1000.25    1.45
+/
+"""
+
+
+def test_lat2device(tmpdir):
+    """
+    Test completor with a two-branch well.
+
+    The lateral of the well, A1, is connected to the main branch via the device layer.
+
+    1. One active well
+    2. Multi-lateral well with two branches
+    """
+    tmpdir.chdir()
+    case_file = f"""
+    -- Case file for testing the LATERAL_TO_DEVICE keyword.
+    SCHFILE
+    ml_well.sch
+    /
+
+    {COMPLETION_TWO_ROWS}
+
+    {USE_STRICT_JOINT_LENGTH}
+
+    SEGMENTLENGTH
+    0.0
+    /
+    {WSEGAICD}
+    -- Lateral 2 in well A1 is routed to the device layer in lateral 1.
+    LATERAL_TO_DEVICE
+    --WELL  LATERAL
+        A1        2
+    /
+    """
+    schedule_file = Path(_TESTDIR / "ml_well.sch")
+    true_file = Path(_TESTDIR / "lat2device.true")
+    common.open_files_run_create(case_file, schedule_file, _TEST_FILE)
+    common.assert_results(true_file, _TEST_FILE)
+
+
+def test_lat2device_non_existing(tmpdir):
