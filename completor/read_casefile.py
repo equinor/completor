344703ef3a2b52c178c6456f51a598172d4cc890
Aleksander Mats Karlsson <AMKA@equinor.com>
2021-12-09 08:56:37 +0100

    rewritten-history-with-blame

--- completor/read_casefile.py
+++ completor/read_casefile.py
@@ -1,3 +1,12 @@
 from __future__ import annotations
 
 import re
+from collections.abc import Mapping
+from io import StringIO
+
+import numpy as np
+import pandas as pd
+
+from completor import input_validation as val
+from completor import parse
+from completor.completion import WellSchedule
