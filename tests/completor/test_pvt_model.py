140400863f0805039d4a1aeab45da11111e31177
Aleksander Mats Karlsson <AMKA@equinor.com>
2021-12-09 08:56:37 +0100

    rewritten-history-with-blame

--- tests/completor/test_pvt_model.py
+++ tests/completor/test_pvt_model.py
@@ -2,18 +2,3 @@
 
 from pathlib import Path
 
-from completor import pvt_model  # type: ignore
-
-_TESTDIR = Path(__file__).absolute().parent / "data"
-
-
-def test_correlation_udq():
-    """Test the function which gives the AICV UDQ expression of SUWCT."""
-    expected = """UDQ
---Water cut definition
-DEFINE SUWCT NINT(SWHF*100/(SWHF+SOHF+1e-20))/100 /
-/
-
-
-"""
-    assert expected == pvt_model.CORRELATION_UDQ
