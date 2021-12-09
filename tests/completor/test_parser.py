1c29d6d37331efdfb9b4ec72f303b85463c06220
Aleksander Mats Karlsson <AMKA@equinor.com>
2021-12-09 08:56:37 +0100

    rewritten-history-with-blame

--- tests/completor/test_parser.py
+++ tests/completor/test_parser.py
@@ -2,28 +2,3 @@
 
 from pathlib import Path
 
-import numpy as np
-
-from completor import parse  # type: ignore
-
-_TESTDIR = Path(__file__).absolute().parent / "data"
-
-
-def test_unpack_record():
-    """
-    Test unpack_records handles eclipse multiple values correctly.
-
-    E.g. 3* should be unpacked to 1*, 1*, 1*, and 2*10 unpacked to 10, 10.
-    """
-    tested = ["3", "2*", "1", "6*"]
-    true = ["3", "1*", "1*", "1", "1*", "1*", "1*", "1*", "1*", "1*"]
-    assert parse.unpack_records(tested) == true
-
-
-def test_locate_keyword():
-    """Test locate_keyword find the correct start and end indexes of keywords."""
-    test1 = ["COMPDAT", "1 2 3 /", "/", "COMPDAT", "2 3 4 /", "/"]
-
-    start_compdat, end_compdat = parse.locate_keyword(test1, "COMPDAT", "/", take_first=False)
-    np.testing.assert_array_equal(start_compdat, [0, 3])
-    np.testing.assert_array_equal(end_compdat, [2, 5])
