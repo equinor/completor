59b5c27ec7e3760439bd17e36eb5f430c9c69100
Aleksander Mats Karlsson <AMKA@equinor.com>
2022-03-10 10:19:39 +0100

    rewritten-history-with-blame

--- tests/completor/test_create_wells.py
+++ tests/completor/test_create_wells.py
@@ -19,3 +19,7 @@ def test_duplicates(tmpdir):
     """Test completor case with duplicated entries in COMPDAT and COMPSEGS.
     Completor produces a number for the second COMPDAT entry, but it is a mistake."""
     tmpdir.chdir()
+    case_file = Path(_TESTDIR / "duplicate.case")
+    schedule_file = Path(_TESTDIR / "duplicate.sch")
+    true_file = Path(_TESTDIR / "duplicate.true")
+    common.open_files_run_create(case_file, schedule_file, _TEST_FILE)
