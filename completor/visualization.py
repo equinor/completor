bf4b0a6f2e3f7edd8724e89016e2ca0954951787
Stefan Crummenerl <37296211+WestfalNamur@users.noreply.github.com>
2022-09-07 11:01:54 +0200

    rewritten-history-with-blame

--- completor/visualization.py
+++ completor/visualization.py
@@ -3,3 +3,6 @@ from __future__ import annotations
 import matplotlib.pyplot as plt  # type: ignore
 import matplotlib.ticker as ticker  # type: ignore
 from matplotlib import rcParams
+from matplotlib.axes import Axes  # type: ignore
+from matplotlib.backends.backend_pdf import PdfPages  # type: ignore
+from matplotlib.figure import Figure  # type: ignore
