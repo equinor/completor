fc569f83915587fb77c3e52f42dec58766d1ba9d
Aleksander Mats Karlsson <AMKA@equinor.com>
2023-01-05 11:51:24 +0100

    rewritten-history-with-blame

--- tests/completor/test_prepare_outputs.py
+++ tests/completor/test_prepare_outputs.py
@@ -7,3 +7,71 @@ import common
 import numpy as np
 import pandas as pd
 
+from completor import completion, prepare_outputs, read_casefile  # type:ignore
+
+_TESTDIR = Path(__file__).absolute().parent / "data"
+_TEST_FILE = "test.sch"
+
+
+def test_trim_pandas():
+    """Test the function to trim pandas data frames with default values."""
+    df_test = pd.DataFrame([[1, 2, 3, "1*", 5, 6], [1, 2, "5*", 3, 4, 5]])
+    df_true = pd.DataFrame(
+        [
+            [1, 2, 3, "1*", 5, 6],
+            [1, 2, "5*", 3, 4, 5],
+        ]
+    )
+    df_test = prepare_outputs.trim_pandas(df_test)
+    pd.testing.assert_frame_equal(df_test, df_true)
+
+    df_test = pd.DataFrame(
+        [
+            ["1*", 2, 3, "1*", 5, "5*"],
+            [1, 2, "5*", 3, 4, "20*"],
+        ]
+    )
+    df_true = pd.DataFrame(
+        [
+            ["1*", 2, 3, "1*", 5],
+            [1, 2, "5*", 3, 4],
+        ]
+    )
+    df_test = prepare_outputs.trim_pandas(df_test)
+    pd.testing.assert_frame_equal(df_test, df_true)
+
+    df_test = pd.DataFrame([["1*", 2, 3, "1*", "2*", "5*"], [1, 2, "5*", 3, "1*", "20*"]])
+    df_true = pd.DataFrame([["1*", 2, 3, "1*"], [1, 2, "5*", 3]])
+    df_test = prepare_outputs.trim_pandas(df_test)
+    pd.testing.assert_frame_equal(df_test, df_true)
+
+
+def test_add_columns_first_last():
+    """Test the function which adds first and last column."""
+    df_test = pd.DataFrame([[1, 2, 3, "1*", 5, 6], [1, 2, "5*", 3, 4, 5]], columns=["A", "B", "C", "D", "E", "F"])
+    df_true_first_last = pd.DataFrame(
+        [[" ", 1, 2, 3, "1*", 5, 6, "/"], [" ", 1, 2, "5*", 3, 4, 5, "/"]],
+        columns=["--", "A", "B", "C", "D", "E", "F", ""],
+    )
+    df_true_only_first = pd.DataFrame(
+        [[" ", 1, 2, 3, "1*", 5, 6], [" ", 1, 2, "5*", 3, 4, 5]], columns=["--", "A", "B", "C", "D", "E", "F"]
+    )
+
+    df_test_first_last = prepare_outputs.add_columns_first_last(df_test, True, True)
+    df_test_only_first = prepare_outputs.add_columns_first_last(df_test, True, False)
+    pd.testing.assert_frame_equal(df_test_first_last, df_true_first_last)
+    pd.testing.assert_frame_equal(df_test_only_first, df_true_only_first)
+
+
+def test_dataframe_to_string():
+    """Test the function which converts data frame to string."""
+    df_test_common = pd.DataFrame(
+        [[1, 2, 3, "1*", 5, 6], [1, 2, "5*", 3, 4, 5]], columns=["A", "B", "C", "D", "E", "F"]
+    )
+    df_true_common = pd.DataFrame(
+        [[" ", 1, 2, 3, "1*", 5, 6, "/"], [" ", 1, 2, "5*", 3, 4, 5, "/"]],
+        columns=["--", "A", "B", "C", "D", "E", "F", ""],
+    )
+
+    df_test_default = prepare_outputs.dataframe_tostring(df_test_common)
+    df_true_default = df_true_common.to_string(index=False, justify="justify")
