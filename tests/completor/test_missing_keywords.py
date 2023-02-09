dac7585723fcf8607e83e9746634eac998bb490a
Trygve Kløv <36536100+tklov11@users.noreply.github.com>
2023-02-09 13:30:47 +0100

    rewritten-history-with-blame

--- tests/completor/test_missing_keywords.py
+++ tests/completor/test_missing_keywords.py
@@ -5,3 +5,9 @@ from pathlib import Path
 import common
 import pytest
 
+COMPLETION = """
+COMPLETION
+--Well Branch Start End Screen   Well/   Roughness Annulus Nvalve/ Valve Device
+--     Number  MD   MD  Tubing   Casing            Content Joint   Type  Number
+--                      Diameter Diameter
+   A1    1     0  10000  0.15364  0.1905 1.524E-005   GP     1   __TYPE__   1
