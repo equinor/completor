b6b20f58930b248b0900efc76b7e207f8f110129
Trygve Kløv <36536100+tklov11@users.noreply.github.com>
2023-02-09 13:30:47 +0100

    rewritten-history-with-blame

--- tests/completor/test_case2ob.py
+++ tests/completor/test_case2ob.py
@@ -15,3 +15,11 @@ def test_perf(tmpdir):
     The roughness and ID of the overburden segment do not match between the files.
     """
     tmpdir.chdir()
+    case_file = """
+-- Case file for testing that overburden segments are described with data from the
+-- case file and not the input schedule file.
+COMPLETION
+--WELL Branch  Start End Screen   Well/  Roughness Annulus Nvalve  Valve  Device
+--     Number  mD    mD  Tubing   Casing           Content /Joint  Type   Number
+--                       Diameter Diameter
+A1      1      0   99999 0.10     0.2159    0.00300   GP   1       AICD   1
