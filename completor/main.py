fce8af8f526f2a3534414f02342e382a16557b65
Trygve Kløv <36536100+tklov11@users.noreply.github.com>
2023-02-09 11:12:24 +0100

    rewritten-history-with-blame

--- completor/main.py
+++ completor/main.py
@@ -555,3 +555,5 @@ def main() -> None:
         if inputs.schedulefile is None:
             raise ValueError("No schedule provided, or none where found " "in the case file (keyword 'SCHFILE')")
         inputs.outputfile = inputs.schedulefile.split(".")[0] + "_advanced.wells"
+
+    paths_input_schedule = (inputs.inputfile, inputs.schedulefile)
