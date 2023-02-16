e120d19adcc61b7f3356a701e63f19ac238be27c
Trygve Kløv <36536100+tklov11@users.noreply.github.com>
2023-02-16 12:15:04 +0100

    rewritten-history-with-blame

--- tests/completor/test_input_validation.py
+++ tests/completor/test_input_validation.py
@@ -21,3 +21,15 @@ def test_lat2dev_with_annulus(tmpdir):
         with open(Path(_TESTDIR / "case_lat2dev.testfile"), encoding="utf-8") as case_file:
             ReadCasefile(case_file.read())
     assert e.type == SystemExit
+
+
+def test_validate_minimum_segment_length():
+    """
+    Test the validate_minimum_segment_length function.
+    """
+
+    with pytest.raises(SystemExit):
+        validate_minimum_segment_length("DUMMY")
+
+    with pytest.raises(SystemExit):
+        validate_minimum_segment_length(-5.0)
