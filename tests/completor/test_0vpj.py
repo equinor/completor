f8d165eec1f754120f52b85d78ea52926e5ee874
Aleksander Mats Karlsson <AMKA@equinor.com>
2023-03-06 13:29:35 +0100

    rewritten-history-with-blame

--- tests/completor/test_0vpj.py
+++ tests/completor/test_0vpj.py
@@ -1,1 +1,7 @@
 """Both GP and OA types of annuli should block flow from the reservoir
+to the devicelayer or annulus layer in 0 VPJ intervals. The 0 VPJ option should
+also work both for cells-, fixed-, user- and welsegs type of segmentation methods."""
+
+from pathlib import Path
+
+import common
