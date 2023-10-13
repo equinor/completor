45b4a2f477d57667f665099c21c7499642e661fe
Torbjørn Wilson <107121928+TorbjornWilson@users.noreply.github.com>
2023-10-13 12:47:13 +0200

    rewritten-history-with-blame

--- completor/logger.py
+++ completor/logger.py
@@ -10,3 +10,21 @@ from pathlib import Path
 
 def getLogger(module_name="completor"):
     logger = logging.getLogger(module_name)
+
+    formatter = logging.Formatter("%(levelname)s:%(name)s:%(message)s")
+
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.addFilter(lambda record: record.levelno < logging.ERROR)
+    stdout_handler.setFormatter(formatter)
+
+    stderr_handler = logging.StreamHandler(sys.stderr)
+    stderr_handler.addFilter(lambda record: record.levelno >= logging.ERROR)
+    stderr_handler.setFormatter(formatter)
+
+    logger.addHandler(stdout_handler)
+    logger.addHandler(stderr_handler)
+
+    return logger
+
+
+logger = getLogger(__name__)
