a1e3d266925e7ffb6812f9145d27969922b8e475
Handita Sutoyo <89432756+hrsut@users.noreply.github.com>
2024-01-24 14:19:23 +0100

    rewritten-history-with-blame

--- setup.py
+++ setup.py
@@ -4,3 +4,14 @@ from __future__ import annotations
 
 from glob import glob
 from os.path import basename, splitext
+
+import setuptools
+from setuptools import find_packages
+
+SSCRIPTS = ["completor = completor.main:main"]
+
+LEGACYSCRIPTS: list[str] = []
+
+REQUIREMENTS = [
+    "matplotlib",
+    "numpy<2",
