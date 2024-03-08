41a34be825f000b4fdce667ecff895622c40ea5c
Handita Sutoyo <89432756+hrsut@users.noreply.github.com>
2024-03-08 15:07:39 +0100

    rewritten-history-with-blame

--- tests/completor/test_create_wells.py
+++ tests/completor/test_create_wells.py
@@ -42,3 +42,45 @@ def test_segment_creation_method():
 COMPLETION
 A1  1     0   2000  0.150  0.216  0.123  GP  0  ICD  1
 A1  1  2000  99999  0.150  0.216  0.123  GP  0  ICD  1
+/
+WSEGSICD
+1  0.00123  1234.00  1.23  0.1
+/
+USE_STRICT
+TRUE
+/
+JOINTLENGTH
+14.0
+/
+SEGMENTLENGTH
+0
+/
+"""
+    # Test default value
+    well = replace_segment_length_value(base_case, "")
+    assert well.method == SegmentCreationMethod.CELLS
+
+    # Test string values
+    well = replace_segment_length_value(base_case, "User")
+    assert well.method == SegmentCreationMethod.USER
+
+    well = replace_segment_length_value(base_case, "WELseGs")
+    assert well.method == SegmentCreationMethod.WELSEGS
+
+    well = replace_segment_length_value(base_case, "infill")
+    assert well.method == SegmentCreationMethod.WELSEGS
+
+    well = replace_segment_length_value(base_case, "Cell")
+    assert well.method == SegmentCreationMethod.CELLS
+
+    # Test number values (i.e. it can be cast to float)
+    well = replace_segment_length_value(base_case, "22")
+    assert well.method == SegmentCreationMethod.FIX
+
+    well = replace_segment_length_value(base_case, "-1")
+    assert well.method == SegmentCreationMethod.USER
+
+    well = replace_segment_length_value(base_case, "0")
+    assert well.method == SegmentCreationMethod.CELLS
+
+    with pytest.raises(ValueError):
