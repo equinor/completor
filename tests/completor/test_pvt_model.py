c8905d0b4e66af685050b16f99b2aacf1efdb3a0
Handita Sutoyo <89432756+hrsut@users.noreply.github.com>
2024-03-08 15:07:39 +0100

    rewritten-history-with-blame

--- tests/completor/test_pvt_model.py
+++ tests/completor/test_pvt_model.py
@@ -9,3 +9,11 @@ _TESTDIR = Path(__file__).absolute().parent / "data"
 
 def test_correlation_udq():
     """Test the function which gives the AICV UDQ expression of SUWCT."""
+    expected = """UDQ
+--Water cut definition
+DEFINE SUWCT NINT(SWHF*100/(SWHF+SOHF+1e-20))/100 /
+/
+
+
+"""
+    assert expected == pvt_model.CORRELATION_UDQ
