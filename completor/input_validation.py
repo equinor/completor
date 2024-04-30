bbfab9a043b7a69b1b2043028a0aa0cc237a9d9b
Aleksander Mats Karlsson <AMKA@equinor.com>
2024-04-30 13:20:27 +0200

    Fix future warning

--- completor/input_validation.py
+++ completor/input_validation.py
@@ -75,8 +75,8 @@ def check_default_non_packer(df_comp: pd.DataFrame) -> pd.DataFrame:
     """
     df_comp = df_comp.copy(True)
     # set default value of roughness
-    df_comp["ROUGHNESS"].replace("1*", 1e-5, inplace=True)
-    df_nonpa: pd.DataFrame = df_comp[df_comp["ANNULUS"] != "PA"]
+    df_comp["ROUGHNESS"] = df_comp["ROUGHNESS"].replace("1*", 1e-5)
+    df_nonpa = df_comp[df_comp["ANNULUS"] != "PA"]
     df_columns = df_nonpa.columns.to_numpy()
     for column in df_columns:
         if "1*" in df_nonpa[column]:
@@ -219,7 +219,7 @@ def set_format_wsegsicd(df_temp: pd.DataFrame) -> pd.DataFrame:
     """
     # if WCUT is defaulted then set to 0.5
     # the same default value as in Eclipse
-    df_temp["WCUT"].replace("1*", 0.5, inplace=True)
+    df_temp["WCUT"] = df_temp["WCUT"].replace("1*", 0.5)
     # set data type
     df_temp["DEVICENUMBER"] = df_temp["DEVICENUMBER"].astype(np.int64)
     # left out devicenumber because it has been formatted as integer
